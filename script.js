0ec9ebb41148c43b96d28d57fd6c47d6a0eaf114
mahesa <157023462+l33tm4n@users.noreply.github.com>
2025-04-28 13:13:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
+const noBtn = document.querySelector(".no yahaha-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, I like you too";
+  question.innerHTML = "Aaaaa, I like you too princess";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
 
 noBtn.addEventListener("mouseover", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+  const maxX = window.innerWidth - no yahahaBtnRect.width;
+  const maxY = window.innerHeight - no yahahaBtnRect.height;
 
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
 
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
+  no yahahaBtn.style.left = randomX + "px";
+  no yahahaBtn.style.top = randomY + "px";
 });
