cf6b312b7b7eaae34c63b1d77ff10a4fe35cbe8b
mahesa <157023462+l33tm4n@users.noreply.github.com>
2025-04-28 13:19:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ yesBtn.addEventListener("click", () => {
 
 noBtn.addEventListener("mouseover", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - no yahahaBtnRect.width;
-  const maxY = window.innerHeight - no yahahaBtnRect.height;
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
 
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
